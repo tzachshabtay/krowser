c046b4fda0ea7232ec6de81f52030726c0c52eb5
Tzach Shabtay <tshabtay@clearstreet.io>
2020-05-27 00:00:42 -0400

    test- add nested fields, unions and multiple subjects in a topic

--- test/publisher.js
+++ test/publisher.js
@@ -1,25 +1,23 @@
 const axios = require('axios')
 const fs = require('fs')
 const { Kafka } = require('kafkajs')
-const { SchemaRegistry, AvroKafka } = require('@ovotech/avro-kafkajs');
+const KafkaAvro = require('kafka-avro');
 
-const raw = fs.readFileSync('test.avsc', 'utf8')
-const testSchema = JSON.parse(raw)
+const kafkaAvro = new KafkaAvro({
+    kafkaBroker: 'localhost:9092',
+    schemaRegistry: 'http://localhost:8081',
+    valueSubjectStrategy: 'RecordNameStrategy',
+});
 
-const schemaRegistry = new SchemaRegistry({ uri: 'http://localhost:8081' })
-const kafka = new Kafka({
-    clientId: 'test-producer',
-    brokers: ['localhost:9092']
-})
-const avroKafka = new AvroKafka(schemaRegistry, kafka)
+const testRaw = fs.readFileSync('test.avsc', 'utf8')
 
-const producer = avroKafka.producer()
+const nestedTestRaw = fs.readFileSync('test_nested.avsc', 'utf8')
 
-const registerSchema = async () => {
+const registerSchema = async (raw, name) => {
     const data = {}
     data["schema"] = raw
     try {
-        const res = await axios.post('http://localhost:8081/subjects/test/versions', data)
+        const res = await axios.post(`http://localhost:8081/subjects/${name}/versions`, data)
         console.log(`Status: ${res.status}`)
         console.log('Body: ', res.data)
     }
@@ -28,20 +26,45 @@ const registerSchema = async () => {
     }
 };
 
-const produceMessage = async () => {
-    await producer.connect()
-    await producer.send({
-        topic: 'test',
-        schema: testSchema,
-        messages: [
-            { value: { long_field: 10, int_field: 20, enum_field: 'option1' } }
-        ],
-    });
+class test { }
+class test_nested { }
+
+const produceTestMessage = async (i) => {
+    const producer = await kafkaAvro.getProducer()
+    const data = new test()
+    data.long_field = 10
+    data.int_field = i
+    data.enum_field = 'option1'
+    producer.produce('test', -1, data, 'key');
+}
+
+const produceNestedTestMessage = async (i) => {
+    const producer = await kafkaAvro.getProducer()
+    const data = new test_nested()
+    data.nullable_long = { "long": 5 }
+    data.nested = {
+        nullable_long: { "long": 5 },
+        int_field: i,
+        boolean_field: true,
+        string_field: "test",
+        nested2: {
+            nullable_long: { "long": 5 },
+            int_field: 20,
+            boolean_field: true,
+            string_field: "test",
+        }
+    }
+    producer.produce('test', -1, data, 'key');
 }
 
 const start = async () => {
-    await registerSchema()
-    await produceMessage()
+    await registerSchema(testRaw, `test-value`)
+    await registerSchema(nestedTestRaw, `test_nested-value`)
+    await kafkaAvro.init()
+    for (let i = 0; i < 5; i++) {
+        await produceTestMessage(i)
+        await produceNestedTestMessage(i)
+    }
 }
 
 start()
