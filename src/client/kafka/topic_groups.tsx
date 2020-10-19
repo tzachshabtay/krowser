9cf7fdf66d7a6c079186e8a7558dceaab1fa8e83
Tzach Shabtay <tshabtay@clearstreet.io>
2020-10-19 00:55:28 -0400

    add slider to consumer group grid

--- src/client/kafka/topic_groups.tsx
+++ src/client/kafka/topic_groups.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Slider from '@material-ui/core/Slider';
 import { KafkaToolbar } from '../common/toolbar';
+import { CellProps } from '../common/cell_button';
 import { DataView } from '../common/data_view';
 import { RouteComponentProps } from "react-router-dom";
+import Box from '@material-ui/core/Box';
 import { ErrorMsg} from '../common/error_msg';
 import { Url } from "../common/url";
 
@@ -12,6 +15,27 @@ type State = {
     error: any;
     rows: any[];
     data: any;
+}
+
+class ProgressRenderer extends React.Component<CellProps, {}> {
+    render() {
+        let offset = this.props.data.offset
+        if (offset === -1) {
+            offset = 0
+        }
+        return (
+        <Box minWidth={150} paddingTop={1} paddingX={3}>
+            <Slider
+                value={offset}
+                aria-labelledby="fake"
+                valueLabelDisplay="off"
+                step={1}
+                min={this.props.data.low}
+                max={this.props.data.high}
+            />
+        </Box>
+        )
+    }
 }
 
 export class TopicGroups extends React.Component<RouteComponentProps<{ topic: string }>, State> {
@@ -44,7 +68,7 @@ export class TopicGroups extends React.Component<RouteComponentProps<{ topic: st
                     partition: partition.partition,
                     offset,
                     high,
-                    low: partition.partitionOffsets.low,
+                    low: parseInt(partition.partitionOffsets.low),
                     lag,
                 })
             }
@@ -57,6 +81,7 @@ export class TopicGroups extends React.Component<RouteComponentProps<{ topic: st
         return [
             { headerName: "Name", field: "name" },
             { headerName: "Partition", field: "partition", filter: "agNumberColumnFilter" },
+            { headerName: "Progress", cellRendererFramework: ProgressRenderer },
             { headerName: "Offset", field: "offset", filter: "agNumberColumnFilter" },
             { headerName: "Low", field: "low", filter: "agNumberColumnFilter" },
             { headerName: "High", field: "high", filter: "agNumberColumnFilter" },
