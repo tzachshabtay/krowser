7f4a62f00dda9fa2701dcea35565d24dd6a45cb3
Tzach Shabtay <tshabtay@clearstreet.io>
2022-02-28 21:58:11 -0500

    kafka connect- add retries

--- src/server/common/errors.rs
+++ src/server/common/errors.rs
@@ -1,5 +1,6 @@
 use std::time::Duration;
 use std::thread;
+use async_std::task;
 
 pub fn map_error<T, E: std::fmt::Debug>(result: Result<T, E>) -> Result<T, String> {
     match result {
@@ -27,3 +28,25 @@ pub fn retry<T, E: std::fmt::Debug>(name: &str, action: &mut dyn std::ops::FnMut
         }
     }
 }
+
+pub async fn retry_async<Fut, T, E: std::fmt::Debug>(name: &str, action: impl FnOnce() -> Fut + Copy) -> Result<T, String>
+    where Fut: std::future::Future<Output = std::result::Result<T, E>>{
+    let mut retries = 5;
+    let mut wait = 500;
+    loop {
+        let res = action().await;
+        match res {
+            Ok(val) => { return Ok(val); },
+            Err(err) => {
+                if retries <= 0 {
+                    return Err(format!("{:?}", err));
+                }
+                eprintln!("Failed {}, retrying in {} milliseconds, retries left {}", name, wait, retries);
+                task::sleep(Duration::from_millis(wait)).await;
+                retries -= 1;
+                wait *= 2;
+            }
+        }
+    }
+}
+
